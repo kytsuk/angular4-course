8d4d3e797277c11e40283f03b161ef1ee8f8f1fd
kytsuk <kytsuk_ua@mail.ru>
2017-05-28 22:25:08 +0300

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipes.module";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe ('Test 1' , 'Description test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+  new Recipe ('Test 1' , 'Description test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
